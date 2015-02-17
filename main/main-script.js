ebe76481e7fd106cd37cd6abb8cf9ef89dcac998
xkun.xu@gmail.com <simba>
2015-02-16 23:23:07 -0500

    Major change in UI

--- main/main-script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.flip').click(function(){
-        $(this).find('.card').addClass('flipped').mouseleave(function(){
-            $(this).removeClass('flipped');
-        });
-        return false;
-    });
-});
