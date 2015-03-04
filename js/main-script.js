27950a363c48cd2a37decd819f6c9cbdf36ea812
YangXue <sumanxy18@gmail.com>
2015-03-04 16:40:48 -0500

    Href and scrolling down added
    *Add href to the tabs in navbar
    *Add scrolling down animation when click tabs

--- js/main-script.js
+++ js/main-script.js
@@ -1,8 +1,35 @@
 $(document).ready(function(){
+
+//flipping card animation
     $('.flip').click(function(){
         $(this).find('.card').addClass('flipped').mouseleave(function(){
             $(this).removeClass('flipped');
         });
         return false;
     });
+//scrolling down animation
+    $("#knowUsTag").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#knowUs").offset().top
+    	}, 500);
+	});
+
+	$("#worshipTag").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#worship").offset().top
+    	}, 500);
+	});
+
+	$("#joinGroupTag").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#joinGroup").offset().top
+    	}, 500);
+	});
+
+	$("#treeTag").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#tree").offset().top
+    	}, 500);
+	});
 });
+
